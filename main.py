ea2301a679b9b04e22e01e65b3ead2230df91edb
pinchiachen <pcc@adenovo.com>
2020-08-26 15:37:28 +0800

    [2020-08-26] refactor

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from time import sleep
-from secret import get_username, get_password, get_target_account
+from secret import get_account
+from target import get_target_account
 
-USERNAME = get_username()
-PASSWORD = get_password()
+USERNAME, PASSWORD = get_account()
 TARGET_ACCOUNT = get_target_account()
+
 SCROLL_PAGE = 1
+BASE_URL = 'https://instagram.com'
 
 class InstagramBot:
     def __init__(self, username, password):
@@ -14,7 +16,7 @@ class InstagramBot:
         self.password = password
 
     def login(self):
-        self.driver.get('https://instagram.com')
+        self.driver.get(BASE_URL)
         sleep(2)
         self.driver.find_element_by_xpath('//input[@name=\"username\"]')\
             .send_keys(self.username)
@@ -34,20 +36,24 @@ class InstagramBot:
         self.driver.get(f'https://instagram.com/{TARGET_ACCOUNT}')
         sleep(2)
 
-    def start(self):
+    def browse(self):
         pic_hrefs = []
-        for i in range(SCROLL_PAGE):
+        for _ in range(SCROLL_PAGE):
             try:
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 hrefs_in_view = self.driver.find_elements_by_tag_name('a')
-                hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view\
-                                    if '.com/p/' in elem.get_attribute('href')]
+                hrefs_in_view = [
+                    elem.get_attribute('href')
+                    for elem in hrefs_in_view
+                    if '.com/p/' in elem.get_attribute('href')
+                ]
                 [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
-                # print(f'pic_hrefs is {pic_hrefs}')
                 sleep(3)
             except Exception:
                 continue
-        
+        return pic_hrefs
+
+    def like_pics(self, pic_hrefs):
         for pic_href in pic_hrefs:
             try:
                 self.driver.get(pic_href)
@@ -56,15 +62,16 @@ class InstagramBot:
                 continue
     
     def like_pic(self): 
-        like = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button/div/span') 
-        like.click()
-        sleep(2) 
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button/div/span')\
+            .click()
+        sleep(2)
 
 def main():
     inst_bot = InstagramBot(USERNAME, PASSWORD)
     inst_bot.login()
     inst_bot.go_target()
-    inst_bot.start()
+    pics = inst_bot.browse()
+    inst_bot.like_pics(pics)
     print('---end---')
 
 if __name__ == "__main__":
